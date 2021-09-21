2359b9149cbdb691202347055cdef3ecb3a8dd30
Ko,Taegyun <dev@weinwork.com>
2021-09-21 17:21:26 +0900

    remove space

--- http_server.hpp
+++ http_server.hpp
@@ -17,33 +17,33 @@ namespace LightContentsDelivery
 	template<class boost::asio::detail::socket_type> class HTTPServerBase
 	{
 		public:
-    HTTPServerBase() {}
+		HTTPServerBase() {}
 		virtual ~HTTPServerBase() {}
 
-    virtual void listen() = 0;
-    virtual void accept() = 0;
-    void send() { }
-    void goodbye() { } // close :D
+		virtual void listen() = 0;
+		virtual void accept() = 0;
+		void send() { }
+		void goodbye() { } // close :D
 		void die() { }
 
 		class Configure
 		{
-      public:
-      int port;
-      int threadPoolSize = 10;
+			public:
+			int port;
+			int threadPoolSize = 10;
 			int timeoutRequest = 10;
 			int timeoutContent = 50;
 
 			Configure(int _port) : port(_port) { }
 		};
 
-    class Content : public std::istream
+		class Content : public std::istream
 		{
 			private:
 			boost::asio::streambuf& streamBuffer;
 
 			public:
-      Content() { }
+			Content() { }
 		};
 
 		class Request
@@ -54,11 +54,11 @@ namespace LightContentsDelivery
 
 		class Response : public std::ostream
 		{
-      private:
+			private:
 			boost::asio::streambuf streamBuffer;
 
-      public:
-      Response() { }
+			public:
+			Response() { }
 		};
 
 		protected:
@@ -68,20 +68,20 @@ namespace LightContentsDelivery
 		void response() { }
 	};
 
-  /**
-   * LightContentsDelivery HTTP Server
-   *
-   * @author TAEGYUN,KO <kotaegyun@weinwork.com>
-   * @date 2021-09-21
-  */
-  typedef boost::asio::ip::tcp::socket HTTP;
-  std::shared_ptr<boost::asio::io_service> ioService;
+	/**
+	 * LightContentsDelivery HTTP Server
+	 *
+	 * @author TAEGYUN,KO <kotaegyun@weinwork.com>
+	 * @date 2021-09-21
+	*/
+	typedef boost::asio::ip::tcp::socket HTTP;
+	std::shared_ptr<boost::asio::io_service> ioService;
 
 	template<> class HTTPServer<HTTP> : public HTTPServerBase<HTTP>
 	{
 		public:
-    HTTPServer() : HTTPServerBase<HTTP>::HTTPServerBase() { /* read config file - yaml type  */ }
-    HTTPServer(int _port) : HTTPServerBase<HTTP>::HTTPServerBase() { configure.port = _port; }
+		HTTPServer() : HTTPServerBase<HTTP>::HTTPServerBase() { /* read config file - yaml type	*/ }
+		HTTPServer(int _port) : HTTPServerBase<HTTP>::HTTPServerBase() { configure.port = _port; }
 		HTTPServer(int _port, int _threadPoolSize = 1, int _timeoutRequest = 5, int _timeoutContent = 300) : HTTPServer()
 		{
 			configure.port = _port;
